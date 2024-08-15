b8a0cca532a2a4352f19d7c1019db9484772adad
kanchanchaudhary37 <chaudharykanchan7100@gmail.com>
2024-08-15 22:42:12 +0545

    portfolio-kanchan

---
+++ js/index.js
@@ -0,0 +1,16 @@
+// class computer
+console.log("i am a javascript")
+  var canvas=document.createElement('p')
+  console.log(canvas)
+
+  var mul = 10*10
+  console.log(mul)
+
+  var createVariable = "hello world"
+  window.alert(createVariable
+  )
+
+  var  num = 10
+  var add = num + 5
+  var name = "kanchan" + "chaudhary"
+  console.log(name)
